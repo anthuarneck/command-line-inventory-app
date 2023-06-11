02df5829b62075f1225182e731c80a29e9007932
Anthony Huarneck <anthonyhuarneck@anthonys-air.myfiosgateway.com>
2023-06-11 11:41:49 -0400

    Updates controller file along with run function on index.js file.

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
 const { writeJSONFile, readJSONFile } = require("./src/helpers");
-const { 
-    create, 
-    index,
-    show,
-    destroy,
-    update,
-} = require("./src/pokemon-cards-controller")
+const {
+  create,
+  index,
+  show,
+  destroy,
+  update,
+} = require("./src/pokemon-cards-controller");
 
 const run = () => {
-    const action = process.argv[2];
-    const card = process.argv[3];
-    let cardsData = readJSONFile(".", "data/pokemon-cards-data.json")
-    let writeToFile = false;
-    let updatedCardsData = [];
-    switch (action) {
-        case "index" :
-            const allCards = index(cardsData)
-            console.log(allCards);
-            break;
-        case "create" :
-            console.log("CREATING RECIEPT FOR PURCHASE")
-            updatedCardsData = create(cardsData, card) 
-            writeToFile = true;
-            break;
-        case "show" :
-            const foundCard = show(cardsData, card)
-            console.log(foundCard)
-            break ;  
+  const action = process.argv[2];
+  const card = process.argv[3];
+  let cardsData = readJSONFile(".", "data/pokemon-cards-data.json");
+  let currentCards = readJSONFile(".", "data/pokemon-cards.json");
+  let writeToFile = false;
+  let updatedCardsData = [];
+  switch (action) {
+    case "index":
+      const allCards = index(cardsData, currentCards);
+      console.log(allCards);
+      break;
+    case "create":
+      console.log("CREATING RECIEPT FOR PURCHASE");
+      updatedCardsData = create(cardsData, card);
+      writeToFile = true;
+      break;
+    case "show":
+      const foundCard = show(cardsData, card);
+      console.log(foundCard);
+      break;
 
-        case "update" :
-            console.log(card,  "updating...")
-            updatedCardsData = update(cardsData, card, process.argv[4]);
-            writeToFile = true;
-            break; 
+    case "update":
+      console.log(card, "updating...");
+      updatedCardsData = update(cardsData, card, process.argv[4]);
+      writeToFile = true;
+      break;
 
-        case "destroy" :
-            updatedCardsData = destroy(cardsData, card);
-            writeToFile = true;
-            break ;
-        default :
-        console.log("error detected")  
-    }
+    case "destroy":
+      updatedCardsData = destroy(cardsData, card);
+      writeToFile = true;
+      break;
+    default:
+      console.log("error detected");
+  }
 
-    if (writeToFile) {
-        console.log("new data detected - updating...")
-  
-        writeJSONFile(".", "data/pokemon-cards-data.json", updatedCardsData)
-    }
+  if (writeToFile) {
+    console.log("new data detected - updating...");
 
-}
-run()
+    writeJSONFile(".", "data/pokemon-cards-data.json", updatedCardsData);
+  }
+};
+run();
--- src/pokemon-cards-controller.js
+++ src/pokemon-cards-controller.js
@@ -3,54 +3,52 @@ const { faker } = require("@faker-js/faker");
 const { chalk } = require("chalk");
 
 function create(cards, cardName) {
-    const card = {
-        id: nanoid(4),
-        name: cardName,
-        priceInCents: faker.commerce.price({ min: 1.00, max: 25.00, dec: 2 }),
-        inStock: faker.datatype.boolean(),
-        holographic: faker.datatype.boolean(),
-    }
-    cards.push(card)
-    return cards
+  const card = {
+    id: nanoid(4),
+    name: cardName,
+    priceInCents: faker.commerce.price({ min: 1.0, max: 25.0, dec: 2 }),
+    inStock: faker.datatype.boolean(),
+    holographic: faker.datatype.boolean(),
+  };
+  cards.push(card);
+  return cards;
 }
 
-function index(cards) {
-    return cards.map((card) => card.id + " " + card.name).join("\n")
+function index(cards, inventory) {
+  return inventory.map((inv) => inv.id + " " + inv.name).join("\n") + "\n" + cards.map((card) => card.id + " " + card.name).join("\n")
 }
 
 function show(cards, cardId) {
-    const foundCard = cards.find((card) => card.id === cardId);
+  const foundCard = cards.find((card) => card.id === cardId);
 
-    return foundCard.id + " " + foundCard.name + " " + foundCard.inStock
+  return foundCard.id + " " + foundCard.name + " " + foundCard.inStock;
 }
 
 function destroy(cards, cardId) {
-    const index = cards.findIndex((card) => card.id === cardId);
-    if (index >= 0) {
-        cards.splice(index, 1);
-        return cards
-    } else {
-        console.log("card with that ID cannot be found")
-    }
+  const index = cards.findIndex((card) => card.id === cardId);
+  if (index >= 0) {
+    cards.splice(index, 1);
+    return cards;
+  } else {
+    console.log("card with that ID cannot be found");
+  }
 }
 
-function update (cards, cardId, updatedCard) {
-    const index = cards.findIndex((card) => card.id === cardId);
-
-    if (index > -1) {
-        cards[index].id = cardId;
-        cards[index].name = updatedCard;
-        cards[index].priceInCents = cardPriceInCents[updatedCard];
-        return cards
-    } else {
-        console.log("Could not find a card with that ID")
-    }
+function update(cards, cardId, updatedCard) {
+  const index = cards.findIndex((card) => card.id === cardId);
+  if (index > -1) {
+    cards[index].id = cardId;
+    cards[index].name = updatedCard;
+    return cards;
+  } else {
+    console.log("Could not find a card with that ID");
+  }
 }
 
 module.exports = {
-    create,
-    index, 
-    show,
-    destroy,
-    update
- }
+  create,
+  index,
+  show,
+  destroy,
+  update,
+};
