7979d6a199ada5cea1af6e7b42d01363ddc6d948
Anthony Huarneck <anthonyhuarneck@anthonys-air.myfiosgateway.com>
2023-06-11 09:27:12 -0400

    Completes index & show functions. Edits create function & adds data to cards objects.

--- src/pokemon-cards-controller.js
+++ src/pokemon-cards-controller.js
@@ -4,6 +4,7 @@ const { chalk } = require("chalk");
 
 function create(cards, cardName) {
     const card = {
+        id: nanoid(4),
         name: cardName,
         priceInCents: faker.commerce.price({ min: 1.00, max: 25.00, dec: 2 }),
         inStock: faker.datatype.boolean(),
@@ -13,3 +14,12 @@ function create(cards, cardName) {
     return cards
 }
 
+function index(cards) {
+    return cards.map((card) => card.id + " " + card.name).join("\n")
+}
+
+function show(cards, cardId) {
+    const foundCard = cards.find((card) => card.id === cardId);
+
+    return foundCard.id + " " + foundCard.name
+}
